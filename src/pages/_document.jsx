64311cc3b25701f3c2e60868f514e9c969702756
myselfshravan <myselfshravan@gmail.com>
2023-06-03 22:34:22 +0530

    fixed media query

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html className="h-full bg-gray-50 antialiased" lang="en">
       <Head />
-      <body className="flex h-full flex-col">
+      <body className="flex h-full">
         <Main />
         <NextScript />
       </body>
